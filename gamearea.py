f80691b0c6c3bb52df68675fd4a57079eaf3a230
abdallah.sarayrah <abdallah.dev85@gmail.com>
2022-03-24 07:50:54 +0300

    add the screen setup within the game area class

--- gamearea.py
+++ gamearea.py
@@ -1,11 +1,21 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 
 
 class GameArea:
-    def __init__(self, widow_height):
+    def __init__(self, window_width, widow_height):
+        self.widow_width = window_width
         self.widow_height = widow_height
         self.ycor = 40 - (self.widow_height // 2)
+        self.screen = Screen()
+        self.setup_screen()
         self.draw_middle_line()
+
+    def setup_screen(self):
+        self.screen = Screen()
+        self.screen.setup(width=self.widow_width, height=self.widow_height, startx=1)
+        self.screen.bgcolor("black")
+        self.screen.title("Pong Game")
+        self.screen.tracer(False)
 
     def draw_middle_line(self):
         """draw the game area middle line"""
@@ -18,3 +28,8 @@ class GameArea:
             news_turtle.setposition(x=0, y=self.ycor)
             self.ycor += 40
             print(self.ycor)
+
+        self.screen.update()
+
+    def dont_exit(self):
+        self.screen.exitonclick()
--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-from turtle import Screen, Turtle
-
 from gamearea import GameArea
 
-screen = Screen()
-screen.setup(width=1400, height=800, startx=1)
-screen.bgcolor("black")
-screen.title("Pong Game")
-screen.tracer(False)
-
-game_area = GameArea(screen.window_height())
-print(game_area.ycor)
-
-screen.update()
-screen.exitonclick()
+game_area = GameArea(window_width=1400, widow_height=800)
+game_area.dont_exit()
