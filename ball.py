49c8771f7cd82278e9df8692e1608c2473e116eb
AbdallahDev <abdallah.dev85@gmail.com>
2024-09-01 16:21:45 +0300

    the ball bounces when it hits the ceiling or the floor

--- ball.py
+++ ball.py
@@ -1,10 +1,10 @@
 from turtle import Turtle
 
-from global_constants import BALL_SHAPE, STRETCH_WID, STRETCH_LEN
+from global_constants import BALL_SHAPE, STRETCH_WID, STRETCH_LEN, BALL_CEILING_HIT_LIMIT, BALL_FLOOR_HIT_LIMIT
 
 
 class Ball(Turtle):
-    """"Represents the ball movement"""
+    """"Represents the ball movement"""""
 
     def __init__(self):
         super().__init__()
@@ -16,14 +16,24 @@ class Ball(Turtle):
                        stretch_len=STRETCH_LEN)
 
     def move(self):
-        """"Moves the ball"""
+        """"Moves the ball"""""
         self.goto(
             self.xcor() + (self.x_direction * 2),
             self.ycor() + (self.y_direction * 1),
         )
-        print(self.x_direction, self.y_direction)
+        self.check_wall_hit()
 
     def bounce(self, x_direction=1, y_direction=1):
-        """"Bounces the ball"""
+        """"Bounces the ball"""""
         self.x_direction = x_direction
         self.y_direction = y_direction
+
+        print(self.x_direction, self.y_direction)
+
+    def check_wall_hit(self):
+        """Check if the ball hits the wall, and if that happens it bounces the ball"""""
+        if self.ycor() > BALL_CEILING_HIT_LIMIT:
+            self.bounce(x_direction=self.x_direction, y_direction=-1)
+
+        if self.ycor() < BALL_FLOOR_HIT_LIMIT:
+            self.bounce(x_direction=self.x_direction, y_direction=1)
