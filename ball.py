44a2e7d6c85ae26012ec7061f6abcfd319b1653f
abdallah.sarayrah <abdallah.dev85@gmail.com>
2024-09-02 11:53:25 +0300

    created constant values related the ball class

--- ball.py
+++ ball.py
@@ -2,7 +2,8 @@ from turtle import Turtle
 
 from global_constants import BALL_SHAPE, BALL_STRETCH_WID, BALL_STRETCH_LEN, BALL_CEILING_HIT_LIMIT, \
     BALL_FLOOR_HIT_LIMIT, BALL_DEFAULT_SPEED, RIGHT_GOAL_HIT, LEFT_GOAL_HIT, BALL_DIRECTION_DEFAULT_VALUE, \
-    BALL_INCREASE_SPEED_VALUE
+    BALL_INCREASE_SPEED_VALUE, BALL_DIRECTION_NEGATION_VALUE, BALL_DIRECTION_MAINTAIN_VALUE, \
+    BALL_CENTERING_DEFAULT_POSITION
 
 
 class Ball(Turtle):
@@ -27,8 +28,8 @@ class Ball(Turtle):
         )
 
         # checks if the ball hits the left paddle or the right paddle
-        r_paddle.hit_ball(self, x_direction=-1)
-        l_paddle.hit_ball(self, x_direction=+1)
+        r_paddle.hit_ball(self, x_direction=BALL_DIRECTION_NEGATION_VALUE)
+        l_paddle.hit_ball(self, x_direction=BALL_DIRECTION_MAINTAIN_VALUE)
 
         self.check_wall_hit()
         if self.check_goal_conceded(r_scoreboard, l_scoreboard):
@@ -47,7 +48,7 @@ class Ball(Turtle):
         """Check if the ball hits the wall, and if that happens it bounces the ball"""""
         if self.ycor() > BALL_CEILING_HIT_LIMIT or self.ycor() < BALL_FLOOR_HIT_LIMIT:
             # I'll negate the y direction to bounce the ball from the wall
-            self.y_direction *= -1
+            self.y_direction *= BALL_DIRECTION_NEGATION_VALUE
             self.bounce(x_direction=self.x_direction, y_direction=self.y_direction)
 
     def check_goal_conceded(self, r_scoreboard, l_scoreboard):
@@ -63,14 +64,14 @@ class Ball(Turtle):
                 if r_scoreboard.update_score():
                     return True
 
-            self.x_direction *= -1
+            self.x_direction *= BALL_DIRECTION_NEGATION_VALUE
             self.center_reposition()
             self.bounce(x_direction=self.x_direction, y_direction=self.y_direction)
 
     def center_reposition(self):
         """It will respawn the ball from the center, and reset the ball speed to default value"""""
         self.speed = BALL_DEFAULT_SPEED
-        self.goto(x=0, y=0)
+        self.goto(BALL_CENTERING_DEFAULT_POSITION)
 
     def increase_speed(self, increase_speed=True):
         """Increases the ball speed"""
